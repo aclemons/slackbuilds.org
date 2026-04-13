0cfc42d201eab604caee5264f47b623e6bf1a712
aclemons <346774+aclemons@users.noreply.github.com>
2026-04-13 04:21:00 +0000

    graphics/vuescan: Updated for version 9.8.56.
    Signed-off-by: Andrew Clemons <aclemons@slackbuilds.org>

--- network/prosody/doinst.sh
+++ network/prosody/doinst.sh
@@ -10,14 +10,6 @@ config() {
 
 config etc/prosody/prosody.cfg.lua.new
 config etc/prosody/migrator.cfg.lua.new
-config etc/prosody/certs/openssl.cnf.new
-config etc/prosody/certs/localhost.key.new
-config etc/prosody/certs/example.com.key.new
-config etc/prosody/certs/example.com.crt.new
-config etc/prosody/certs/GNUmakefile.new
-config etc/prosody/certs/makefile.new
-config etc/prosody/certs/localhost.cnf.new
-config etc/prosody/certs/localhost.crt.new
 
 if ! [ -L etc/rc.d/rc.prosody ] ;
 then
